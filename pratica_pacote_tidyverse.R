f87c415ed7fe3a8881c08e2ba4a5f94790c60923
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-14 22:30:45 -0300

    Add files via upload

--- pratica_pacote_tidyverse.R
+++ pratica_pacote_tidyverse.R
@@ -12,3 +12,19 @@ library(tidyverse)
 
 ping <- dados::pinguins
 View(ping)
+
+# Análises descritivas ---------------------------------------------------------------------------------------------------------------------
+
+### Medidas de comprimento da nadadeira, massa corporal e dimensões do bico por
+### sexo de pinguins no ano de 2009
+
+names(ping) # Verificar nomes das variáveis
+ping$ano # Verificar quais anos incluídos nos dados
+
+ping1 <- ping %>%
+  select(comprimento_bico, profundidade_bico, comprimento_nadadeira,
+         massa_corporal, sexo, ano) %>%
+  filter(ano == "2009") %>%
+  view()
+ping1
+
